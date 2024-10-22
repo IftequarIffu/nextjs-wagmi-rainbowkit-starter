a29dab52445e8be69201c58cb8f39dac24b3f989
IftequarIffu <iftequar.154@gmail.com>
2024-10-22 23:08:45 +0530

    Connected with smart contract using wagmi

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,36 @@
+'use client'
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { useAccount } from 'wagmi';
+import { useReadContract } from 'wagmi'
+import { abi } from '@/lib/constants';
 
 
 export default function Home() {
+
+  const {address} = useAccount()
+
+  const listingPrice = useReadContract({
+    abi,
+    address: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
+    functionName: 'getListingPrice'
+  }).data
+
+  const nftContractAddress = useReadContract({
+    abi,
+    address: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
+    functionName: 'getBasicNftContractAddress'
+  }).data
+
   return (
-    <div className='flex justify-end'>
-      <ConnectButton />
+    <div>
+      <nav className='flex justify-end p-4'>
+        <ConnectButton />
+      </nav>
+      <div>
+        <p>{`Address: ${address}`}</p>
+        <p>{`Listing Price: ${listingPrice}`}</p>
+        <p>{`NFT Contract Address: ${nftContractAddress}`}</p>
+      </div>
     </div>
   );
 }
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,107 @@
 export const WALLETCONNECT_PROJECT_ID = "97c8f6f0aea6cdcc6f66203f64fd1bc5"
+export const NFT_MARKETPLACE_CONTRACT_ADDRESS = "0x34A1D3fff3958843C43aD80F30b94c510645C316"
+
+export const abi = [
+        {
+            "type": "constructor",
+            "inputs": [
+                {
+                    "name": "listingPrice",
+                    "type": "uint256",
+                    "internalType": "uint256"
+                }
+            ],
+            "stateMutability": "nonpayable"
+        },
+        {
+            "type": "function",
+            "name": "getBasicNftContractAddress",
+            "inputs": [],
+            "outputs": [
+                {
+                    "name": "",
+                    "type": "address",
+                    "internalType": "address"
+                }
+            ],
+            "stateMutability": "view"
+        },
+        {
+            "type": "function",
+            "name": "getListingPrice",
+            "inputs": [],
+            "outputs": [
+                {
+                    "name": "",
+                    "type": "uint256",
+                    "internalType": "uint256"
+                }
+            ],
+            "stateMutability": "view"
+        },
+        {
+            "type": "function",
+            "name": "listNft",
+            "inputs": [
+                {
+                    "name": "tokenId",
+                    "type": "uint256",
+                    "internalType": "uint256"
+                }
+            ],
+            "outputs": [],
+            "stateMutability": "payable"
+        },
+        {
+            "type": "function",
+            "name": "s_basicNftContractAddress",
+            "inputs": [],
+            "outputs": [
+                {
+                    "name": "",
+                    "type": "address",
+                    "internalType": "address"
+                }
+            ],
+            "stateMutability": "view"
+        },
+        {
+            "type": "function",
+            "name": "s_listingPrice",
+            "inputs": [],
+            "outputs": [
+                {
+                    "name": "",
+                    "type": "uint256",
+                    "internalType": "uint256"
+                }
+            ],
+            "stateMutability": "view"
+        },
+        {
+            "type": "function",
+            "name": "s_owner",
+            "inputs": [],
+            "outputs": [
+                {
+                    "name": "",
+                    "type": "address",
+                    "internalType": "address payable"
+                }
+            ],
+            "stateMutability": "view"
+        },
+        {
+            "type": "function",
+            "name": "updateListingPrice",
+            "inputs": [
+                {
+                    "name": "newListingPrice",
+                    "type": "uint256",
+                    "internalType": "uint256"
+                }
+            ],
+            "outputs": [],
+            "stateMutability": "nonpayable"
+        }
+    ] as const
