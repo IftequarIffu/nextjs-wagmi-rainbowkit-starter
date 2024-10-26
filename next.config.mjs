b4d5822062a2cf2273aa58db4aeac79f3b749969
IftequarIffu <iftequar.154@gmail.com>
2024-10-26 13:40:24 +0530

    Added ipfs.io in next config to allow image fetching

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'ipfs.io', // Replace with the domain of the external website
+                port: '',
+                pathname: '/ipfs/**', // Adjust the path if needed
+            },
+        ],
+    },
+};
 
 export default nextConfig;
