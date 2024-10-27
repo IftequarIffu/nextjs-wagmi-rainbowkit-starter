4bbd05ee181a6b765f2c8698cbf28e4be567298a
IftequarIffu <iftequar.154@gmail.com>
2024-10-27 20:17:46 +0530

    Made changes to marketplace

--- app/marketplace/page.tsx
+++ app/marketplace/page.tsx
@@ -144,7 +144,7 @@ export default function Marketplace() {
 
           {/* NFT Grid */}
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-            {myLikedNfts?.map((nft: any) => (
+            {listedNfts?.map((nft: any) => (
             //   <Card key={nft.id} className="overflow-hidden">
             //     <CardHeader className="p-0">
             //       <div className="relative aspect-square">
