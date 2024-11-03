41bbf2df9ccc23c01bccdd37321c0d2321ce5c3f
IftequarIffu <iftequar.154@gmail.com>
2024-11-03 20:49:47 +0530

    Cleaned Purchases page

--- app/purchases/page.tsx
+++ app/purchases/page.tsx
@@ -3,31 +3,15 @@
 'use client'
 
 import * as React from 'react'
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { PlusCircle, LayoutDashboard, ShoppingBag, Search, X, Heart } from "lucide-react"
-import Image from "next/image"
-import Link from "next/link"
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuItem,
-  SidebarMenuButton,
-  SidebarProvider,
-  SidebarRail,
   SidebarTrigger,
   SidebarInset,
 } from '@/components/ui/sidebar'
 import { ThemeToggle } from '@/components/theme-toggle'
 import { useAccount } from 'wagmi';
-import MintNftModal from '@/components/MintNftModal'
 import NftCard from '@/components/NftCard'
-import { useReadContract, useWriteContract, useSimulateContract } from 'wagmi'
-import { BASIC_NFT_CONTRACT_ADDRESS, basicNftAbi, marketPlaceAbi } from '@/lib/constants'
-import { useQueryClient } from '@tanstack/react-query'
+import { useReadContract } from 'wagmi'
+import { basicNftAbi, marketPlaceAbi } from '@/lib/constants'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import DashboardPagesSidebarProvider from '@/components/DashboardPagesSidebarProvider'
 
@@ -70,20 +54,10 @@ export default function Favorites() {
     abi: basicNftAbi,
     address: basicNftAddress,
     functionName: 'getMyPurchasedNftsTokenIds',
-    // args: [BigInt(0)]
     account: address
   })
 
   console.log("My Purchases TokenIDs: ", myPurchasedNftsTokenIds)
-
-
-
-  // const purchasedNfts = myNfts?.filter((nft) => {
-  //   return nft.isSold && nft.owner == address
-  // })
-
-
-
 
   return (
     <DashboardPagesSidebarProvider>
@@ -91,16 +65,6 @@ export default function Favorites() {
           <header className="flex h-16 items-center gap-4 border-border px-6 w-full">
             <SidebarTrigger />
             <div className="flex flex-1 items-center space-x-4">
-              <form className="flex-1">
-                {/* <div className="relative">
-                  <Search className="absolute left-2.5 top-2.5 h-4 w-4" />
-                  <Input
-                    type="search"
-                    placeholder="Search NFTs..."
-                    className="w-full pl-8 md:w-2/3 lg:w-1/3"
-                  />
-                </div> */}
-              </form>
             </div>
             <ConnectButton />
             <ThemeToggle />
@@ -113,7 +77,6 @@ export default function Favorites() {
                     <h1 className="mb-8 text-2xl font-bold">My Purchases</h1>
                     <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
                     
-
                     {myPurchasedNftsTokenIds?.map( (tokenId) => (
                         
                         <NftCard nftsTokenId={tokenId}/>
